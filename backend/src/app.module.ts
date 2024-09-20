531c88d944a43dfa8d2d2a7135a7c8897cf58af5
Duong Quang Minh <minh2811quang@gmail.com>
2024-09-20 23:55:46 +1000

    Re-establish a backend database

---
+++ backend/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5437,
+      password: 'postgres',
+      username: 'postgres',
+      entities: [User],
+      database: 'Focus_Bear_Hackathon_Database',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
